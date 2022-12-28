5f99fce6cebee134adc90e3f8e56c58336200fca
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2022-12-28 14:44:01 +0300

    Reset to dev

--- tpl_radicalmart_uikit/html/layouts/components/radicalmart/payment/error.php
+++ tpl_radicalmart_uikit/html/layouts/components/radicalmart/payment/error.php
@@ -2,7 +2,7 @@
 /*
  * @package     RadicalMart Package
  * @subpackage  com_radicalmart
- * @version     1.0.0
+ * @version     __DEPLOY_VERSION__
  * @author      Delo Design - delo-design.ru
  * @copyright   Copyright (c) 2021 Delo Design. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
--- tpl_radicalmart_uikit/html/layouts/plugins/radicalmart_fields/standard/field/filter/checkboxes.php
+++ tpl_radicalmart_uikit/html/layouts/plugins/radicalmart_fields/standard/field/filter/checkboxes.php
@@ -2,7 +2,7 @@
 /*
  * @package     RadicalMart Uikit Package
  * @subpackage  tpl_radicalmart_uikit
- * @version     1.0.0
+ * @version     __DEPLOY_VERSION__
  * @author      Delo Design - delo-design.ru
  * @copyright   Copyright (c) 2022 Delo Design. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
