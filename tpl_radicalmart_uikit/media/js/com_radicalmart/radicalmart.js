ffdc574c7e36b2c7a8bc50cc45c55623b90a332a
Igor Berdichevskiy <septdir@gmail.com>
2024-06-27 13:48:56 +0300

    Prepare 2.1.0 Release

--- tpl_radicalmart_uikit/media/js/com_radicalmart/radicalmart.js
+++ tpl_radicalmart_uikit/media/js/com_radicalmart/radicalmart.js
@@ -1,7 +1,7 @@
 /*
  * @package     RadicalMart Uikit Package
  * @subpackage  tpl_radicalmart_uikit
- * @version     __DEPLOY_VERSION__
+ * @version     2.1.0
  * @author      RadicalMart Team - radicalmart.ru
  * @copyright   Copyright (c) 2024 RadicalMart. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
