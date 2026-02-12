bc008cdb7df7a9650d27284ef59e273d6de68b1b
Septdir <iberdichevskiy@septdir.com>
2026-02-12 16:25:07 +0300

    bonuses codes

--- tpl_radicalmart_uikit/html/com_radicalmart/category/alphabetical.php
+++ tpl_radicalmart_uikit/html/com_radicalmart/category/alphabetical.php
@@ -18,7 +18,7 @@ use Joomla\Utilities\ArrayHelper;
 
 // Load assets
 /** @var \Joomla\CMS\WebAsset\WebAssetManager $assets */
-$assets = $this->document->getWebAssetManager();
+$assets = $this->getDocument()->getWebAssetManager();
 if ($this->params->get('trigger_js', 1))
 {
 	$assets->useScript('com_radicalmart.site.trigger');
--- tpl_radicalmart_uikit/html/com_radicalmart_bonuses/codes/default.php
+++ tpl_radicalmart_uikit/html/com_radicalmart_bonuses/codes/default.php
@@ -15,11 +15,12 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\Component\RadicalMart\Administrator\Helper\PriceHelper;
-use Joomla\Utilities\ArrayHelper;
+
+/** @var \Joomla\Component\RadicalMartBonuses\Site\View\Codes\HtmlView $this */
 
 // Load assets
 /** @var \Joomla\CMS\WebAsset\WebAssetManager $assets */
-$assets = $this->document->getWebAssetManager();
+$assets = $this->getDocument()->getWebAssetManager();
 $assets->getRegistry()->addExtensionRegistryFile('com_radicalmart');
 if ($this->params->get('radicalmart_js', 1))
 {
@@ -35,14 +36,14 @@ $currenciesCount = count(PriceHelper::getCurrencies());
 ?>
 <div id="RadicalMartBonuses" class="bonuses-codes">
 	<div class="uk-child-width-expand@m uk-grid-medium" uk-grid>
-		<div class="uk-width-1-4@m">
+		<div class="uk-width-medium@m uk-flex-last uk-flex-first@m">
 			<?php echo LayoutHelper::render('components.radicalmart.account.sidebar'); ?>
 			<?php if (!empty($this->modules['radicalmart-account-sidebar'])): ?>
-				<div class="mt-3">
+				<div class="uk-margin">
 					<?php foreach ($this->modules['radicalmart-account-sidebar'] as $module): ?>
-						<div class="mb-3">
+						<div class="uk-margin">
 							<?php if ($module->showtitle): ?>
-								<div class="h3"><?php echo Text::_($module->title); ?></div>
+								<div class="uk-h3"><?php echo Text::_($module->title); ?></div>
 							<?php endif; ?>
 							<div><?php echo $module->render; ?></div>
 						</div>
@@ -51,101 +52,73 @@ $currenciesCount = count(PriceHelper::getCurrencies());
 			<?php endif; ?>
 		</div>
 		<div>
-			<div class="uk-card uk-card-default uk-card-small">
-				<div class="uk-card-header">
-					<h1 class="uk-h2">
-						<?php echo $this->params->get('seo_bonuses_codes_h1',
-							($this->menuCurrent) ? $this->menu->title : Text::_('COM_RADICALMART_BONUSES_CODES')); ?>
-					</h1>
+			<h1 class="uk-h2 uk-margin uk-margin-remove-top">
+				<?php echo $this->params->get('seo_bonuses_codes_h1',
+						($this->menuCurrent) ? $this->menu->title : Text::_('COM_RADICALMART_BONUSES_CODES')); ?>
+			</h1>
+			<?php if (empty($this->items)): ?>
+				<div class="uk-alert uk-alert-warning">
+					<?php echo Text::_('COM_RADICALMART_BONUSES_CODES_NO_ITEMS'); ?>
 				</div>
-				<?php if (empty($this->items)): ?>
-					<div class="uk-card-body">
-						<div class="uk-alert uk-alert-warning">
-							<?php echo Text::_('COM_RADICALMART_BONUSES_CODES_NO_ITEMS'); ?>
+			<?php else: ?>
+				<?php foreach ($this->items as $i => $item): ?>
+					<div class="uk-card uk-card-small uk-card uk-card-default uk-card-body uk-margin
+					<?php if (!$item->enabled) echo 'uk-alert-danger'; ?>">
+						<div class="uk-flex uk-flex-middle uk-grid-small" uk-grid>
+							<div class="uk-width-1-2 uk-width-expand@m">
+								<div class="uk-text-meta">
+									<?php echo Text::_('COM_RADICALMART_BONUSES_CODE'); ?>
+								</div>
+								<div>
+									<code><?php echo $item->code; ?></code>
+								</div>
+							</div>
+							<div class="uk-width-1-2 uk-width-small@m">
+								<div class="uk-text-meta">
+									<?php echo Text::_('COM_RADICALMART_PRICE_DISCOUNT'); ?>
+								</div>
+								<div><?php echo $item->discount_string; ?></div>
+							</div>
+							<div class="uk-width-1-2 uk-width-1-4@m">
+								<?php if (!empty($item->expires) && $item->expires !== '0000-00-00 00:00:00'): ?>
+									<div class="uk-text-meta">
+										<?php echo Text::_('COM_RADICALMART_BONUSES_CODE_CREATED'); ?>
+									</div>
+									<div>
+										<?php echo HTMLHelper::date($item->created, Text::_('DATE_FORMAT_LC5')); ?>
+									</div>
+								<?php endif; ?>
+							</div>
+							<div class="uk-width-1-2 uk-width-1-4@m">
+								<?php if (!empty($item->expires) && $item->expires !== '0000-00-00 00:00:00'): ?>
+									<div class="uk-text-meta">
+										<?php echo Text::_('COM_RADICALMART_BONUSES_CODE_EXPIRES'); ?>
+									</div>
+									<div>
+										<?php echo HTMLHelper::date($item->expires, Text::_('DATE_FORMAT_LC5')); ?>
+									</div>
+								<?php endif; ?>
+							</div>
+							<div class="uk-width-1-1 uk-flex uk-flex-middle">
+								<div class="uk-text-meta uk-margin-small-right">
+									<?php echo Text::_('COM_RADICALMART_ORDERS') . ': '; ?>
+								</div>
+								<div>
+									<?php foreach ($item->orders as $order): ?>
+										<a href="<?php echo $order->link ?>" class="uk-margin-small-right">
+											<?php echo $order->number; ?>
+										</a>
+									<?php endforeach; ?>
+								</div>
+							</div>
 						</div>
 					</div>
-				<?php else: ?>
-					<div class="uk-card-body">
-						<table class="uk-table uk-table-divider uk-table-responsive uk-table-middle">
-							<thead class="uk-visible@m">
-							<tr>
-								<th class="uk-text-nowrap">
-									<?php echo Text::_('COM_RADICALMART_BONUSES_CODE'); ?>
-								</th>
-								<th class="uk-text-nowrap">
-									<?php echo Text::_('COM_RADICALMART_PRICE_DISCOUNT'); ?>
-								</th>
-								<?php if ($currenciesCount > 1): ?>
-									<th class="uk-text-nowrap">
-										<?php echo Text::_('COM_RADICALMART_CURRENCY'); ?>
-									</th>
-								<?php endif; ?>
-								<th class="uk-text-nowrap">
-									<?php echo Text::_('COM_RADICALMART_ORDERS'); ?>
-								</th>
-								<th class="uk-text-nowrap">
-									<?php echo Text::_('COM_RADICALMART_BONUSES_CODE_CREATED'); ?>
-								</th>
-								<th class="uk-text-nowrap">
-									<?php echo Text::_('COM_RADICALMART_BONUSES_CODE_CREATED_BY'); ?>
-								</th>
-							</tr>
-							</thead>
-							<tbody>
-							<?php foreach ($this->items as $i => $item) : ?>
-								<tr class="<?php if (!$item->enabled) echo 'uk-alert-danger'; ?>">
-									<td class="text-nowrap">
-										<span class="uk-hidden@m">
-											<?php echo Text::_('COM_RADICALMART_BONUSES_CODE'); ?>:
-										</span>
-										<code><?php echo $item->code; ?></code>
-									</td>
-									<td>
-										<span class="uk-hidden@m">
-											<?php echo Text::_('COM_RADICALMART_PRICE_DISCOUNT'); ?>:
-										</span>
-										<?php echo $item->discount_string; ?>
-									</td>
-									<?php if ($currenciesCount > 1): ?>
-										<td class="uk-text-small">
-											<span class="uk-hidden@m">
-												<?php echo Text::_('COM_RADICALMART_CURRENCY'); ?>:
-											</span>
-											<?php echo Text::_($item->currency['title']); ?>
-										</td>
-									<?php endif; ?>
-									<td class="uk-text-small">
-										<span class="uk-hidden@m">
-											<?php echo Text::_('COM_RADICALMART_ORDERS'); ?>:
-										</span>
-										<?php if (!empty($item->orders))
-										{
-											echo implode(', ', ArrayHelper::getColumn($item->orders, 'number'));
-										} ?>
-									</td>
-									<td class="uk-text-small uk-text-nowrap">
-										<span class="uk-hidden@m">
-											<?php echo Text::_('COM_RADICALMART_BONUSES_CODE_CREATED'); ?>:
-										</span>
-										<?php echo HTMLHelper::date($item->created, Text::_('DATE_FORMAT_LC5')); ?>
-									</td>
-									<td class="uk-text-small">
-										<span class="uk-hidden@m">
-											<?php echo Text::_('COM_RADICALMART_BONUSES_CODE_CREATED_BY'); ?>:
-										</span>
-										<?php echo $item->created_by->name; ?>
-									</td>
-								</tr>
-							<?php endforeach; ?>
-							</tbody>
-						</table>
+				<?php endforeach; ?>
+				<?php if ($this->pagination): ?>
+					<div class="list-pagination uk-margin-medium">
+						<?php echo $this->pagination->getPaginationLinks(); ?>
 					</div>
 				<?php endif; ?>
-			</div>
-			<?php if ($this->items && $this->pagination): ?>
-				<div class="list-pagination uk-margin-medium">
-					<?php echo $this->pagination->getPaginationLinks(); ?>
-				</div>
 			<?php endif; ?>
 		</div>
 	</div>
