074bef7e18de4c0a214f6468425675a409126a83
Septdir <iberdichevskiy@septdir.com>
2026-04-20 14:10:37 +0300

    Reset to dev

--- tpl_radicalmart_uikit/html/layouts/plugins/radicalmart/bonuses_points/field/points.php
+++ tpl_radicalmart_uikit/html/layouts/plugins/radicalmart/bonuses_points/field/points.php
@@ -2,7 +2,7 @@
 /*
  * @package     RadicalMart Bonuses Package
  * @subpackage  com_radicalmart_bonuses
- * @version     3.0.16
+ * @version     __DEPLOY_VERSION__
  * @author      RadicalMart Team - radicalmart.ru
  * @copyright   Copyright (c) 2026 RadicalMart. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
